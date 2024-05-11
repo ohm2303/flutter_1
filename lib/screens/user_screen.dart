ebd621f42933762ceef47b815617551af4cc91a6
tanawat.k <tanawat.k@orbitdigital.co.th>
2024-05-11 15:55:43 +0700

    fix: add image.

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,5 +1,9 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/elements/texts/small_text.dart';
+import '../widgets/elements/texts/text_title.dart';
 
 class UserPageScreen extends StatefulWidget {
   const UserPageScreen({super.key});
@@ -15,33 +19,32 @@ class _UserPageScreenState extends State<UserPageScreen> {
       appBar: AppBar(
         title: const Text('User Screen'),
       ),
-      body: Center(
-        child: Column(
+      body: const Center(
+        child:  Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(
-                  'https://www.google.com/imgres?q=doraemon&imgurl=https%3A%2F%2Fvariety.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fdoraemon2.jpg%3Fcrop%3D0px%252C193px%252C1593px%252C887px%26resize%3D1000%252C563&imgrefurl=https%3A%2F%2Fvariety.com%2F2014%2Ftv%2Fasia%2Ficonic-japanese-cartoon-doraemon-acquired-by-disney-1201176265%2F&docid=Rp8scMCoK4uAzM&tbnid=9Nw1Kx64B07ZAM&vet=12ahUKEwiY37KWmYWGAxW3z6ACHQ9TBasQM3oECBUQAA..i&w=1000&h=563&hcb=2&ved=2ahUKEwiY37KWmYWGAxW3z6ACHQ9TBasQM3oECBUQAA'),
+            CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://variety.com/wp-content/uploads/2014/05/doraemon2.jpg?crop=0px%2C193px%2C1593px%2C887px&resize=1000%2C563')
             ),
-            const SizedBox(height: 20),
-            Text(
-              'ชื่อผู้ใช้',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            SizedBox(height: 20),
+            TextTitle(
+              title: 'Memo',
+              color: Colors.blue,
             ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // todo something
-              },
-              child: const Text('แก้ไขโปรไฟล์'),
+            SizedBox(height: 10),
+            SizedBox(height: 20),
+            SmallText(
+              title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+                  'Lorem Ipsum has been the industry is standard dummy text ever since the 1500s, '
+                  'when an unknown printer took a galley of type and scrambled it to make a type specimen book. '
+                  'It has survived not only five centuries, but also the leap into electronic typesetting, '
+                  'remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, '
+                  'and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+              color: Colors.black54,
             ),
-            const SizedBox(height: 20),
-            Text(
-              'ข้อมูลเพิ่มเติม',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 10),
+            SizedBox(height: 10),
             Text(
               'รายละเอียดเพิ่มเติมเกี่ยวกับผู้ใช้',
               style: TextStyle(fontSize: 16),
